10478ab76061602513e71b33614d3e5597f826a8
Devin Hurley <snowmiser111@mac.com>
2015-02-08 18:47:42 -0500

    proof Leontief works

--- src/wrangle.py
+++ src/wrangle.py
@@ -24,7 +24,7 @@ def save_table(someFile, theDelimiter=',',startPoint=2,endPoint=17):
     # divide 1.0 by the elements so that when I use this vector in the dot product, I can use dot product and not have to iterate
     # through each element in the matrix.
     q = [1.0/389891,1.0/698562,1.0/240200,1.0/241677,1.0/3711464,1.0/648278,1.0/128054,1.0/644228,1.0/543301,1.0/2218888,1.0/2684780,1.0/72190,1.0/281067,1.0/211354,1.0/86188]
-    r = [1.0/481026,1.0/580284, 1.0/496708, 1.0/1206919, 1.0/5787100, 1.0/1458137, 1.0/1392835, 1.0/1024517, 1.0/1223897, 1.0/5133698, 1.0/3691586,1.0/2564883,1.0/1218544,1.0/740624,1.0/2706124]
+    x = [1.0/481026,1.0/580284, 1.0/496708, 1.0/1206919, 1.0/5787100, 1.0/1458137, 1.0/1392835, 1.0/1024517, 1.0/1223897, 1.0/5133698, 1.0/3691586,1.0/2564883,1.0/1218544,1.0/740624,1.0/2706124]
     #vec = np.matrix([[item] for item in r])
     afile = open(someFile, 'rb')
     data = csv.reader(afile, delimiter=theDelimiter)
@@ -49,7 +49,7 @@ def save_table(someFile, theDelimiter=',',startPoint=2,endPoint=17):
         i = 0
         row2 = []
         for item in row:
-            item = item*r[i]
+            item = item*x[i]
             row2.append(math.fabs(float(item)))
             i += 1
             #print row2
@@ -63,14 +63,30 @@ def save_table(someFile, theDelimiter=',',startPoint=2,endPoint=17):
     np.save('myMatrix',A)
     print A
     print "printing vec now.."
-    vec = np.matrix([[1.0/float(item)] for item in r])
-    print vec
-    B = A.dot(vec)
+    vecx = np.matrix([[1.0/float(item)] for item in x])
+    print vecx
+    B = A.dot(vecx)
     #B = np.load('myMatrix.npy')
     print("\nB:")
     print B
-    print("\n vec - B")
-    print vec - B
+    print("\n our vector d")
+    vecd = vecx - B
+    print vecx - B
+
+    print ("\nNow we will compute our function to find vecx\n\
+        it was vecx but yolo")
+    print("computing x=(I-C)^-1 . d")
+
+    print "..."
+    # create identity matrix by computing A . inverse(A)
+    I = A.dot(A.I)
+
+    prod = (I - A)
+    result = prod.I
+    print result.dot(vecd)
+    print "The above should be equal to vecx"
+    print vecx
+    
 
 
 
